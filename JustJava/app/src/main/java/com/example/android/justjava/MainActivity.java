daec804296072d1288e2898762271540e69685d8
GK <GK>
2016-04-27 12:48:03 +0530

    Add intent to Gmail - To send the order summary to an email id.

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -65,7 +67,13 @@ public class MainActivity extends AppCompatActivity {
         name = cname.getText().toString();
         int price = calculatePrice(haswhippedCream, haschocolate);
 
-        displayMessage(createOrderSummary(price, haswhippedCream, haschocolate));
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:gkgokul10@gmail.com")); // only email apps should handle this
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+            intent.putExtra(Intent.EXTRA_TEXT,createOrderSummary(price, haswhippedCream, haschocolate));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
@@ -111,11 +119,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
 }
